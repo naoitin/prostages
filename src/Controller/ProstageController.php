3defe3beb9d20fb551330c939bc4c71921763b0b
Antoine EVAIN <antoine.evain64@gmail.com>
2019-11-21 11:02:36 +0100

    Version 2 du tp 1

--- src/Controller/ProstageController.php
+++ src/Controller/ProstageController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
+
+
 class ProstageController extends AbstractController
 {
     /**
@@ -15,35 +17,48 @@ class ProstageController extends AbstractController
 
     public function indexAccueil()
     {
-        return new Response ("<html><head><title>Accueil</title></head>
-                            <body><h1>Bienvenue sur la page d'accueil de Prostages</h1></body></html>");
+        return $this->render('prostage/index_Accueil.html.twig', [
+            'controller_name' => 'ProstageController',
+        ]);
+        /*return new Response ("<html><head><title>Accueil</title></head>
+                           <body><h1>Bienvenue sur la page d'accueil de Prostages</h1></body></html>");*/
     }
     /**
      * @Route("/entreprises", name="prostage_Entreprises")
      */
-    //Route vers l'accueil
+    //Route vers les entreprises
     public function indexEntreprises()
     {
-        return new Response ("<html><head><title></title></head>
-                            <body><h1>Cette page affichera la liste des entreprises proposant un stage</h1></body></html>");
+        return $this->render('prostage/index_Entreprises.html.twig', [
+            'controller_name' => 'ProstageController',
+        ]);
+        /*return new Response ("<html><head><title></title></head>
+                            <body><h1>Cette page affichera la liste des entreprises proposant un stage</h1></body></html>");*/
     }
     /**
      * @Route("/formations", name="prostage_Formations")
      */
-    //Route vers l'accueil
+    //Route vers les formations
     public function indexFormations()
     {
-        return new Response ("<html><head><title>Formations</title></head>
-                            <body><h1>Cette page affichera la liste des formations de l'IUT</h1></body></html>");
+        return $this->render('prostage/index_Formations.html.twig', [
+            'controller_name' => 'ProstageController',
+        ]);
+        /*return new Response ("<html><head><title>Formations</title></head>
+                            <body><h1>Cette page affichera la liste des formations de l'IUT</h1></body></html>");*/
     }
     /**
      * @Route("/stages/{id}", name="prostage_Stages")
      */
-    //Route vers l'accueil
+    //Route vers les stages
     public function indexStages($id)
     {
-        return new Response ("<html><head><title>Stages</title></head>
+        return $this->render('prostage/index_Stages.html.twig', [
+            'id'=>$id
+        ]);
+        /*return new Response ("<html><head><title>Stages</title></head>
                             <body><h1>Cette page affichera le descriptif du stage ayant pour identifiant $id </h1>
-                            </body></html>");
+                            </body></html>");*/
     }
 }
+?>
