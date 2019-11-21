443039ec3760979eb66f66ecd3b068a8f161b72e
Antoine EVAIN <antoine.evain64@gmail.com>
2019-11-21 10:22:00 +0100

    Version 1 du tp 1

--- src/Controller/ProstageController.php
+++ src/Controller/ProstageController.php
@@ -4,17 +4,46 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProstageController extends AbstractController
 {
     /**
-     * @Route("/", name="prostage")
+     * @Route("/", name="prostage_Accueil")
      */
     //Route vers l'accueil
-    public function index()
+
+    public function indexAccueil()
     {
-        return $this->render('prostage/index.html.twig', [
-            'controller_name' => 'ProstageController',
-        ]);
+        return new Response ("<html><head><title>Accueil</title></head>
+                            <body><h1>Bienvenue sur la page d'accueil de Prostages</h1></body></html>");
+    }
+    /**
+     * @Route("/entreprises", name="prostage_Entreprises")
+     */
+    //Route vers l'accueil
+    public function indexEntreprises()
+    {
+        return new Response ("<html><head><title></title></head>
+                            <body><h1>Cette page affichera la liste des entreprises proposant un stage</h1></body></html>");
+    }
+    /**
+     * @Route("/formations", name="prostage_Formations")
+     */
+    //Route vers l'accueil
+    public function indexFormations()
+    {
+        return new Response ("<html><head><title>Formations</title></head>
+                            <body><h1>Cette page affichera la liste des formations de l'IUT</h1></body></html>");
+    }
+    /**
+     * @Route("/stages/{id}", name="prostage_Stages")
+     */
+    //Route vers l'accueil
+    public function indexStages($id)
+    {
+        return new Response ("<html><head><title>Stages</title></head>
+                            <body><h1>Cette page affichera le descriptif du stage ayant pour identifiant $id </h1>
+                            </body></html>");
     }
 }
